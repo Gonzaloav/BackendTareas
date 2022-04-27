a92d38e4a289c4a9b9cd4eb11ec538a8e5b267e7
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-04-27 13:50:39 +0200

    controllers

--- src/controllers/UsersControllers.mjs
+++ src/controllers/UsersControllers.mjs
@@ -1,10 +1,10 @@
 import { users } from "../models/usersModels.mjs";
 
 export function getTaskController (request, response) {
-         response.json(tasks)
+         response.json(tasks).send ('Hola Mundo');
 }
 
 export function postTaskController (request, response) {
     users.push (request.body);
-    response.sendStatus (201); 
+    response.sendStatus (201).send ('Hola Mundo'); 
 }
